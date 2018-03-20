3fd7a7b90634c554dc99c50272a14e3e9dce40c1
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-20 21:58:20 +0400

    Added a bilking text using component state

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
-class Greeteng extends Component {
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isShowingText: false
+        };
+
+        setInterval(() => {
+            this.setState((prevState) => {
+                return {isShowingText: !prevState.isShowingText};
+            });
+        }, 1000);
+    }
+
     render() {
+        let display = this.state.isShowingText ? this.props.text : ' ';
+
         return (
-            <Text>Hello {this.props.name}</Text>
+            <Text>{display}</Text>
         );
     }
 }
 
-export default class LotsOfGreeteng extends Component {
+export default class BlinkApp extends Component {
     render() {
         return (
-            <View style={{alignItems: 'center'}}>
-                <Greeteng name="Gurban"/>
-                <Greeteng name="Famil"/>
-                <Greeteng name="Nurik"/>
+            <View>
+                <Blink text='Blinking string 1'/>
+                <Blink text='Blinking string 2'/>
+                <Blink text='Blinking string 3'/>
             </View>
         );
     }
