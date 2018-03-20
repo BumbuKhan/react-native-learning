f304934016302ca06b9fd7997e0e879dbecc0195
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-20 23:51:46 +0400

    Playing with flexbox

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import {View} from 'react-native';
 
 export default class MyComponent extends Component {
     render() {
-        return(
+        return (
             // flexDirection is defaulted by 'column'
-            <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
                 <View style={{width: 50, height: 50, backgroundColor: 'blue'}}></View>
                 <View style={{width: 50, height: 50, backgroundColor: 'red'}}></View>
                 <View style={{width: 50, height: 50, backgroundColor: 'green'}}></View>
