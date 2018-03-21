33b306236ae223c41f52c44dc5da6f786124bbce
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-21 23:56:22 +0400

    Playing with ScrollView

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import {View, Button, Alert} from 'react-native';
+import {Text, ScrollView} from 'react-native';
 
 export default class ButtonComponent extends Component {
     render() {
         return(
-            <View style={{padding: 10}}>
-                <Button onPress={() => {
-                    Alert.alert('The button has been pressed');
-                }} title="Press me" />
-            </View>
+            <ScrollView>
+                <Text style={{fontSize: 140}}>Lorem ipsum dolor</Text>
+            </ScrollView>
         );
     }
 }
