8ea8428106c756d45f20e6862da1aad03e00d60d
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-22 00:34:15 +0400

    Fetching and displaying some data from the internet

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
 import React, {Component} from 'react';
-import {Text, FlatList, View} from 'react-native';
+import {View, Text, FlatList, ActivityIndicator} from 'react-native';
 
-export default class FlatListBasic extends Component {
-    render() {
-        return (
-            <View>
-                <FlatList data={[
-                    {key: 'Devin'},
-                    {key: 'Jackson'},
-                    {key: 'Mike'},
-                    {key: 'Bob'},
-                ]} renderItem={({item}) => <Text>{item.key}</Text>} />
-            </View>
-        );
+export default class FetchExemple extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {isLoading: true};
     }
+
+    componentDidMount() {
+        return fetch('https://facebook.github.io/react-native/movies.json')
+        .then((response) => response.json())
+        .then((responseJSON) => {
+            this.setState({
+                isLoading: false,
+                dataSource: responseJSON.movies
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    render(){
+
+        if(this.state.isLoading){
+          return(
+            <View style={{flex: 1, padding: 20}}>
+              <ActivityIndicator/>
+            </View>
+          )
+        }
+    
+        return(
+          <View style={{flex: 1, paddingTop:20}}>
+            <FlatList
+              data={this.state.dataSource}
+              renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+              keyExtractor={(item, index) => index}
+            />
+          </View>
+        );
+      }
 }
