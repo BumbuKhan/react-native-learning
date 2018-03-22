176969c90e716dd80aedbbc7180d8fcf479ea106
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-22 12:30:42 +0400

    Added a basic example of using react-navigation package

--- App.js
+++ App.js
@@ -1,45 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text, FlatList, ActivityIndicator} from 'react-native';
+import {View, Text} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
-export default class FetchExemple extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {isLoading: true};
-    }
-
-    componentDidMount() {
-        return fetch('https://facebook.github.io/react-native/movies.json')
-        .then((response) => response.json())
-        .then((responseJSON) => {
-            this.setState({
-                isLoading: false,
-                dataSource: responseJSON.movies
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
-
-    render(){
-
-        if(this.state.isLoading){
-          return(
-            <View style={{flex: 1, padding: 20}}>
-              <ActivityIndicator/>
+class HomeScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Home screen</Text>
             </View>
-          )
-        }
-    
-        return(
-          <View style={{flex: 1, paddingTop:20}}>
-            <FlatList
-              data={this.state.dataSource}
-              renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-              keyExtractor={(item, index) => index}
-            />
-          </View>
         );
-      }
+    }
+} 
+
+const RootStack = StackNavigator({
+    Home: {screen: HomeScreen}
+});
+
+export default class App extends Component {
+    render() {
+        return (
+            <RootStack />
+        );
+    }
 }
