7f7422bbb9962730c32f39be6fc7b4233078fb6b
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-22 12:41:00 +0400

    Added a second screen to navigate to

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 
 class HomeScreen extends Component {
     render() {
         return (
             <View>
-                <Text>Home screen</Text>
+                <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>Home screen</Text>
+                <Button onPress={() => this.props.navigation.navigate('Details')} title="Go to details"/>
+            </View>
+        );
+    }
+}
+
+class DetailsScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>Details screen</Text>
             </View>
         );
     }
 } 
 
-const RootStack = StackNavigator({
-    Home: {screen: HomeScreen}
-});
+const RootStack = StackNavigator(
+    {
+        Home: {screen: HomeScreen},
+        Details: {screen: DetailsScreen}
+    },
+    {
+        initialRouteName: 'Home'
+    }
+);
 
 export default class App extends Component {
     render() {
